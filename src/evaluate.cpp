22380c001d99906516367a83b9da9098fb023efd
Marco Costalba <mcostalba@gmail.com>
2019-02-24 11:30:22 +0100

    KingDanger should be ortogonal to score
    KingDanger is a term added to score, it should not
    rely on the already computed score.
    
    Bench: 3331165

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -479,7 +479,6 @@ namespace {
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  + 150 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
                  - 873 * !pos.count<QUEEN>(Them)
-                 -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
                  +   5 * kingFlankAttacks * kingFlankAttacks / 16
                  -   25;
